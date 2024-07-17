e39ba22a65d14608b1b3153a21348dc4fe34b850
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2024-07-16 23:16:09 -0300

    ADD: register entity docker-compose typeOrm

--- src/register/entities/register.entity.ts
+++ src/register/entities/register.entity.ts
@@ -1,1 +1,31 @@
-export class Register {}
+import { AfterInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Register {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length:30})
+    nombre: string;
+
+    @Column({length: 30})
+    apellido: string;
+
+    @Column({length: 30})
+    nombre_fantasia?: string;
+
+    @Column()
+    telefono: string;
+
+    @AfterInsert()
+    async todoMayusculas() {
+        this.nombre = this.nombre.toUpperCase();
+        this.apellido = this.apellido.toUpperCase();
+        if (this.nombre_fantasia) {
+            this.nombre_fantasia = this.nombre_fantasia.toUpperCase();
+        }
+    }
+
+
+}
